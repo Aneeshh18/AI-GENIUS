fbfd93b4fcf05813a518b2410ca12c1c18bfb187
AzharAhmad007 <azharcr8@bbdnitm.ac.in>
2024-03-10 17:53:16 +0530

    Image, Music and Video Generation created. Now, API limit will be set

---
+++ ai-genius/app/api/video/route.ts
@@ -0,0 +1,64 @@
+import  OpenAI   from "openai";
+import { NextResponse } from "next/server";
+import  Replicate  from "replicate";
+
+
+const openai = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+  });
+
+  async function main() {
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: 'Say this is a test' }],
+      model: 'gpt-3.5-turbo',
+    });
+  }
+  
+  main();
+
+  const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN!
+})
+
+export async function POST(
+    req:Request
+){
+    try{
+        const {userId} = auth()
+        const body = await req.json();
+        const { messages } = body
+
+        if (!userId){
+            return new NextResponse("Unauthorized",{status:401});
+        }
+
+        if (!prompt){
+            return new NextResponse("Message is required",{status:400});
+        }
+
+        const response = await replicate.run(
+            "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+            {
+              input: {
+                fps: 24,
+                model: "xl",
+                width: 1024,
+                height: 576,
+                prompt: prompt,
+                batch_size: 1,
+                num_frames: 24,
+                init_weight: 0.5,
+                guidance_scale: 17.5,
+                remove_watermark: false,
+                num_inference_steps: 50
+              }
+            }
+          );
+
+
+        return NextResponse.json(response);
+    }catch (error){
+        console.log("[VIDEO_ERROR]",error);
+        return new NextResponse("Internal error",{status:500});
+    }
+}
